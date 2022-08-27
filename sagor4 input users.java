02f915c8ac8fa241ed304b90c97dc0b08c7130e5
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 02:44:12 +0600

    Update sagor4 input users.java

--- sagor4 input users.java
+++ sagor4 input users.java
@@ -1,4 +1,4 @@
-package sagor;
+/*package sagor;
 import java.util.Scanner;
 public class sagor4 {
     public static void main(String[] args){
@@ -15,3 +15,26 @@ public class sagor4 {
 
     }
 }
+*/
+
+package sagor;
+import java.util.Scanner;
+public class sagor4 {
+    public static void main(String[] args){
+        System.out.println("Taking input from the users: ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number 1: ");
+        //int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2: ");
+       // int b = sc.nextInt();
+        float b = sc.nextFloat();
+        //int sum = a + b;
+        float sum = a + b;
+        System.out.println("The sum of this number is: ");
+        System.out.println(sum);
+
+
+    }
+}
+
