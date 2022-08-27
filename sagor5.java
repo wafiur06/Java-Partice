ab92025c2d80e87ea2ba4ce6627906ed79f1b9fe
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 02:56:17 +0600

    Add files via upload

---
+++ sagor5.java
@@ -0,0 +1,10 @@
+package sagor;
+
+public class sagor5 {
+    public static void main(String[] args ){
+        // q1 = sum of three numbers;
+        int a = 4,b = 3,c = 2,sum;
+        sum = a + b + c;
+        System.out.println(sum);
+    }
+}
