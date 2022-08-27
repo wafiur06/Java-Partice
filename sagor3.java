cfd54531e7b85212df467f3b0eee197a5ec3e1a5
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 02:04:40 +0600

    Add files via upload

---
+++ sagor3.java
@@ -0,0 +1,20 @@
+package sagor;
+
+public class sagor3 {
+    public static void main(String[] args){
+        byte a = 34;
+        int ag = 39;
+        short ag1 = 23;
+        long aa = 45555555555555l;
+        char bg = 'S';
+        float gb = 5.6f;
+        double di  = 4.555d;
+        System.out.println(a);
+        System.out.println(ag);
+        System.out.println(ag1);
+        System.out.println(aa);
+        System.out.println(bg);
+        System.out.println(gb);
+        System.out.println(di);
+    }
+}
