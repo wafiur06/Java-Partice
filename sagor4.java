32032b8a1b4d97102f8e99d4496605c3fcece934
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 02:45:07 +0600

    Add files via upload

---
+++ sagor4.java
@@ -0,0 +1,26 @@
+package sagor;
+import java.util.Scanner;
+public class sagor4 {
+    public static void main(String[] args){
+        System.out.println("Taking input from the users: ");
+        Scanner sc = new Scanner(System.in);
+        /*(System.out.println("Enter number 1: ");
+        int a = sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.println("Enter number 2: ");
+       // int b = sc.nextInt();
+        float b = sc.nextFloat();
+        //int sum = a + b;
+        float sum = a + b;
+        System.out.println("The sum of this number is: ");
+        System.out.println(sum);*/
+
+        /*boolean b1 = sc.hasNextFloat();
+        System.out.println(b1);*/
+        String sr = sc.nextLine();
+        System.out.println(sr);
+
+
+
+    }
+}
