ff1ffe7a6b69554787b5e757bef4ac64c359f5d1
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 03:04:13 +0600

    Create sagor7 calculate cgpa.java

---
+++ sagor7 calculate cgpa.java
@@ -0,0 +1,11 @@
+package sagor;
+
+public class sagor5 {
+    public static void main(String[] args ){
+        // q1 = sum of three numbers;
+       float sub1 = 45,sub2 = 95,sub3 = 48;
+       float cgpa = (sub1 + sub2 + sub3)/30;
+        System.out.println(cgpa);
+
+    }
+}
