6be09795078f0d66bbe0f3bf7242cc262b302eb2
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-28 01:43:45 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package sagor;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("the sum of this number is: ");
+        int a=1,b=3,c=4;
+        int sum = a + b + c;
+        System.out.println(sum);
+    }
+}
