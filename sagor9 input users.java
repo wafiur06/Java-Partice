1d8eff108db9bba0b15813a95a4a31544bb8c000
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-29 01:47:32 +0600

    Create sagor9 input users.java

---
+++ sagor9 input users.java
@@ -0,0 +1,11 @@
+package sagor;
+
+import java.util.Scanner;
+
+public class sagor6 {
+    public static void main(String[] args){
+        System.out.println("Enter a integer number ");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextDouble());
+    }
+}
