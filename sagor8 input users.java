1ab606f2e1860b4b60afe4389a02e2a9b7965ea0
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-29 01:44:52 +0600

    Create sagor8 input users.java

---
+++ sagor8 input users.java
@@ -0,0 +1,14 @@
+package sagor;
+
+import java.util.Scanner;
+
+public class sagor6 {
+    public static void main(String[] args){
+        System.out.println("What is your name");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Hello " + name + " have a good day!");
+
+     
+    }
+}
