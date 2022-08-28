f70233c529d89fcc06ee8f61ecbceef4f6d4cf29
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-29 02:08:15 +0600

    Create sagor10 Operators.java

---
+++ sagor10 Operators.java
@@ -0,0 +1,25 @@
+package sagor;
+
+import java.util.Scanner;
+
+public class sagor6 {
+    public static void main(String[] args){
+        int a = 4;//arithmetic operator
+        int c = 6 % a;//modulo operator
+        int b=7;
+        b*= 7;//assingment operator
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(64<6);//Comparison Operator
+        System.out.println(66==66);//Comparison Operator
+        System.out.println(54>4 && 4<66);//logical Operator
+        System.out.println(54<4 || 4>66);//logical Operator
+        System.out.println(2 & 3);//bitwise operator
+        //10
+       // 11
+       //-----
+     //   10
+
+
+    }
+}
