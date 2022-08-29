71f89b8cd3e85f04d83f99dbb2156cdb4f2f5125
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-30 02:52:32 +0600

    Create sagor15 printf\format.java

---
+++ sagor15 printf\format.java
@@ -0,0 +1,13 @@
+package sagor;
+
+public class sagor4 {
+    public static void main(String[] args) {
+        String s = "sagor";
+        int a = 4;
+        float b = 6.55555555555555555f;
+        System.out.printf("the value of a is %d\n", a);
+        System.out.format("the value of b is %9.3f\n", b);
+        System.out.printf("the value of s is %s\n", s);
+
+    }
+}
