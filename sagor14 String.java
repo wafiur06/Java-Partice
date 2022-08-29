5cc1a798eeb4d95299fad9088ab029024f05a7d8
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-30 02:35:34 +0600

    Create sagor14 String.java

---
+++ sagor14 String.java
@@ -0,0 +1,9 @@
+package sagor;
+
+public class sagor4 {
+    public static void main(String[] args) {
+        String s = "sagor";
+        System.out.println("the name is: ");
+        System.out.println(s);
+    }
+}
