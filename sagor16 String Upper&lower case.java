0c7e739fc888ccf5cec83db71ceb018144141b65
Md.Wafiur Rahman <108853518+wafiur06@users.noreply.github.com>
2022-08-30 03:04:03 +0600

    Create sagor16 String Upper&lower case.java

---
+++ sagor16 String Upper&lower case.java
@@ -0,0 +1,15 @@
+package sagor;
+
+public class sagor4 {
+    public static void main(String[] args) {
+       String s = "sagor";
+      //String s = "wafi";
+      int b = s.length();
+        System.out.println(b);
+        String e = s.toLowerCase();
+        System.out.println(e);
+        String t = s.toUpperCase();
+        System.out.println(t);
+
+    }
+}
